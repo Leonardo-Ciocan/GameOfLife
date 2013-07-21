d11228652c3fc83042d4f719a87a9e610e0b933c
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2013-07-21 21:07:10 +0100

    Initial Commit

---
+++ GameOfLife/Game.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class Game
+    {
+        public static Cell[,] Universe = new Cell[150, 100];
+
+        public delegate void RedrawHandler();
+        public static event RedrawHandler Redraw;
+
+        public static void InitializeUniverse()
+        {
+            for (int i = 0; i < 150; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    Universe[i,j]=new Cell();
+                }
+            }
+        }
+
+        public static int getAdjecentCells(int x, int y)
+        {
+            int adjecent = 0;
+            if( x+1 < Universe.GetLength(1))
+                if (Game.Universe[x + 1, y].Alive) adjecent++;
+
+            if (y + 1 <  Universe.GetLength(1)) 
+                if (Game.Universe[x, y + 1].Alive) adjecent++;
+
+            if (x + 1 < Universe.GetLength(1) && y + 1 < Universe.GetLength(1))
+                if (Game.Universe[x + 1, y + 1].Alive) adjecent++;
+
+            if (x - 1 >0)
+                if (Game.Universe[x - 1, y].Alive) adjecent++;
+
+            if (y-1 >0) 
+                if (Game.Universe[x, y - 1].Alive) adjecent++;
+
+            if (x + 1 <  Universe.GetLength(1))
+                if (Game.Universe[x - 1, y - 1].Alive) adjecent++;
+
+            if (x + 1 <  Universe.GetLength(1))
+                if (Game.Universe[x - 1, y + 1].Alive) adjecent++;
+
+            if (x + 1 <  Universe.GetLength(1))
+                if (Game.Universe[x + 1, y - 1].Alive) adjecent++;
+            return adjecent;
+        }
+
+
+        public static void CalculateNextGeneration()
+        {
+            Cell[,] NextUniverse = new Cell[150, 100];
+            for (int i = 0; i < 150; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    NextUniverse[i,j]=new Cell();
+                    int adjecent = getAdjecentCells(i, j);
+                    if (Universe[i, j].Alive)
+                    {
+                        if (adjecent < 2 && adjecent > 3) NextUniverse[i, j].Alive = false;
+                        if (adjecent > 1 && adjecent < 4) NextUniverse[i, j].Alive = true;
+                    }
+                    else
+                    {
+                        if (adjecent == 3) NextUniverse[i, j].Alive = true;
+                    }
+
+                }
+            }
+            Universe = NextUniverse;
+            Redraw();
+        }
+    }
+}
---
+++ GameOfLife/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace GameOfLife
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Game.InitializeUniverse();
+            DrawUniverse();
+            Game.Redraw += DrawUniverse;
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromSeconds(0.1);
+            dt.Tick += (a, b) => Game.CalculateNextGeneration();
+            this.KeyDown += (a, b) =>
+            {
+                if (b.Key == Key.Space) dt.Start();
+            };
+        }
+
+        public void DrawUniverse()
+        {
+            root.Children.Clear();
+            for (int i = 0; i < 150; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    var rect = new Rectangle
+                    {
+                        Width =8 ,
+                        Height=8,
+                        Stroke= new SolidColorBrush(Colors.Gray),
+                        StrokeThickness = 0.3
+                    };
+                    rect.Tag = new Point(i, j);
+                    rect.MouseLeftButtonDown += (a, b) =>
+                    {
+                        Point pos = (Point)rect.Tag;
+                        (Game.Universe[(int) pos.X, (int) pos.Y].Alive) =
+                            !(Game.Universe[(int) pos.X, (int) pos.Y].Alive);
+                        rect.Fill = new SolidColorBrush((Game.Universe[(int)pos.X, (int)pos.Y].Alive) ? Colors.Black : Colors.White);
+                    };
+                    rect.Fill = new SolidColorBrush((Game.Universe[i, j].Alive) ?  Colors.Black:Colors.White );
+                    root.Children.Add(rect);
+                    Canvas.SetLeft(rect , i* 8);
+                    Canvas.SetTop(rect, j * 8);
+                }
+            }
+        }
+
+        
+
+        
+    }
+}
