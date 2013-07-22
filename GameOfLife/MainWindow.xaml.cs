fedc66abf022176e90aa896f0197a775bd70d910
Leonardo Ciocan <leonardo.ciocan@outlook.com>
2013-07-22 17:56:50 +0100

    Converted the project to use bitmap instead of vectors to make it much more efficient

--- GameOfLife/MainWindow.xaml.cs
+++ GameOfLife/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -38,59 +39,92 @@ namespace GameOfLife
             btnStart.Click += (a, b) => dt.Start();
             MouseLeftButtonDown += (a, b) => MouseDown = true;
             MouseLeftButtonUp += (a, b) => MouseDown = false;
+
+            root.MouseMove += (a, b) =>
+            {
+                if (MouseDown)
+                {
+                    int i = (int)Math.Round( (b.GetPosition(root).X/8));
+                    int j = (int)Math.Round((b.GetPosition(root).Y/ 8));
+                    UniverseImage.FillRectangle(i*8, j*8, i*8 + 8, j*8 + 8, Colors.Black);
+                    Game.Universe[i, j].Alive = !Game.Universe[i, j].Alive;
+                }
+            };
         }
 
-
-        public CellControl[,] DrawnUniverse = new CellControl[150,100];
+        private WriteableBitmap UniverseImage = new WriteableBitmap(1200,800,96,96, PixelFormats.Pbgra32,null);
         public void DrawInitialUniverse()
         {
-            root.Children.Clear();
-            for (int i = 0; i < 150; i++)
-            {
-                for (int j = 0; j < 100; j++)
-                {
-                    var rect = new CellControl();
-                    rect.Tag = new Point(i, j);
-                    rect.MouseEnter += (a, b) =>
-                    {
-                        if (MouseDown)
-                        {
-                            Point pos = (Point) rect.Tag;
-                            (Game.Universe[(int) pos.X, (int) pos.Y].Alive) =
-                                !(Game.Universe[(int) pos.X, (int) pos.Y].Alive);
-                            rect.AliveMask.Visibility =
-                                (Game.Universe[(int) pos.X, (int) pos.Y].Alive)
-                                    ? Visibility.Visible
-                                    : Visibility.Collapsed;
-                        }
-
-                    };
-                    rect.AliveMask.Visibility =
-                                (Game.Universe[i,j].Alive)
-                                    ? Visibility.Visible
-                                    : Visibility.Collapsed;
-                    root.Children.Add(rect);
-                    DrawnUniverse[i, j] = rect;
-                    Canvas.SetLeft(rect , i* 8);
-                    Canvas.SetTop(rect, j * 8);
-                }
-            }
+            UniverseImage.Clear(Colors.White);
+            root.Background = new ImageBrush(UniverseImage);
         }
 
-        
         public void UpdateUniverse()
         {
+            UniverseImage.Clear(Colors.White);
             for (int i = 0; i < 150; i++)
             {
                 for (int j = 0; j < 100; j++)
                 {
-                    //Color col = (Game.Universe[i, j].Alive) ? Colors.Black : Colors.White;
-                    //if (col != (rect.Fill as SolidColorBrush).Color) rect.Fill = new SolidColorBrush(col);
-                    DrawnUniverse[i, j].AliveMask.Visibility = ((Game.Universe[i, j].Alive)) ? Visibility.Visible : Visibility.Collapsed;
+                    //Color c = Color.FromRgb((byte)Math.Min(255, Math.Floor(Game.Universe[i, j].Generation/1000.0)), 0, 0);
+                    UniverseImage.FillRectangle(i*8, j*8, i*8 + 8, j*8 + 8,
+                        (Game.Universe[i, j].Alive) ? Colors.Black : Colors.White);
                 }
             }
-            //txtGeneration.Text = Game.TotalGenerations.ToString();
         }
+
+#region Alternative Method Using User Controls
+        //public CellControl[,] DrawnUniverse = new CellControl[150,100];
+        //public void DrawInitialUniverse()
+        //{
+        //    root.Children.Clear();
+        //    for (int i = 0; i < 150; i++)
+        //    {
+        //        for (int j = 0; j < 100; j++)
+        //        {
+        //            var rect = new CellControl();
+        //            rect.Tag = new Point(i, j);
+        //            rect.MouseEnter += (a, b) =>
+        //            {
+        //                if (MouseDown)
+        //                {
+        //                    Point pos = (Point) rect.Tag;
+        //                    (Game.Universe[(int) pos.X, (int) pos.Y].Alive) =
+        //                        !(Game.Universe[(int) pos.X, (int) pos.Y].Alive);
+        //                    rect.AliveMask.Visibility =
+        //                        (Game.Universe[(int) pos.X, (int) pos.Y].Alive)
+        //                            ? Visibility.Visible
+        //                            : Visibility.Collapsed;
+        //                }
+
+        //            };
+        //            rect.AliveMask.Visibility =
+        //                        (Game.Universe[i,j].Alive)
+        //                            ? Visibility.Visible
+        //                            : Visibility.Collapsed;
+        //            root.Children.Add(rect);
+        //            DrawnUniverse[i, j] = rect;
+        //            Canvas.SetLeft(rect , i* 8);
+        //            Canvas.SetTop(rect, j * 8);
+        //        }
+        //    }
+        //}
+
         
+        //public void UpdateUniverse()
+        //{
+        //    for (int i = 0; i < 150; i++)
+        //    {
+        //        for (int j = 0; j < 100; j++)
+        //        {
+        //            //Color col = (Game.Universe[i, j].Alive) ? Colors.Black : Colors.White;
+        //            //if (col != (rect.Fill as SolidColorBrush).Color) rect.Fill = new SolidColorBrush(col);
+        //            DrawnUniverse[i, j].AliveMask.Visibility = ((Game.Universe[i, j].Alive)) ? Visibility.Visible : Visibility.Collapsed;
+        //        }
+        //    }
+        //    //txtGeneration.Text = Game.TotalGenerations.ToString();
+        //}
+#endregion
+
     }
 }
